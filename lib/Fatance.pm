ecc6b53e34425396e9d5b3f4d9dfc5e4494302b5
Junichiro Tobe <junichiro.tobe@gmail.com>
2009-06-03 23:33:18 +0900

    initial commit

---
+++ lib/Fatance.pm
@@ -0,0 +1,33 @@
+package Fatance;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Fatance -
+
+=head1 SYNOPSIS
+
+  use Fatance;
+
+=head1 DESCRIPTION
+
+Fatance is
+
+=head1 AUTHOR
+
+junichiro E<lt>junichiro.tobe@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
